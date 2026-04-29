d2091c4b02c90324f2eb781a77368e14664d1f35
Your Name <anjalibommisetty19@gmail.com>
2026-04-29 23:23:40 +0530

    added

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,13 +1,14 @@
 import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
+import { Platform } from 'react-native';
 
-const supabaseUrl = 'https://lrxoalxpyomtcqzueqmb.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxyeG9hbHhweW9tdGNxenVlcW1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0Mjk5ODksImV4cCI6MjA5MzAwNTk4OX0.wQVhjAhT4p7hpifSbRlZaES1KjjtR7PiJkDa13MgViY';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storage: AsyncStorage,
+    storage: AsyncStorage as any,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
